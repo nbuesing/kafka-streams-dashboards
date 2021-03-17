a99815fcd2a7520dac3cbe95e8e4d177372ed9b4
Neil Buesing <neil.buesing@objectpartners.com>
2021-03-16 21:30:24 -0500

    adding druid for optional JMX gathering option for metrics.

---
+++ druid/load.sh
@@ -0,0 +1,15 @@
+
+URL=http://localhost:18081
+#URL=http://druid-coordinator:8081
+
+CONTENT_TYPE_HDR='Content-Type:application/json'
+
+DATA=config.json
+
+curl -X POST -H $CONTENT_TYPE_HDR -d @$DATA $URL/druid/indexer/v1/supervisor
+#curl -X POST http://localhost:18081/druid/indexer/v1/supervisor/_metrics-kafka-streams/shutdown
+
+
+#TASK=task
+#curl -X POST -H $CONTENT_TYPE_HDR -d @$DATA $URL/druid/indexer/v1/$TASK
+
--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -3,3 +3,4 @@
 (cd applications; docker-compose down)
 (cd monitoring; docker-compose down)
 (cd cluster; docker-compose down)
+#docker network rm ksd
